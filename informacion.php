666986be32edd70edd510ca452377ac7652b6b96
NARIASS2023 <128531842+NARIASS2023@users.noreply.github.com>
2023-07-13 21:57:57 +0200

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacion = $_POST['Identificacion'];
+$NomApellidos = $_POST['NomApellidos'];
+$Cargo= $_POST['Cargo'];
+$Salario= $_POST['Salario'];
+hecho "LA IDENTIFICACION DEL EMPLEADO ES: ".$Identificacion;
+hecho "LOS NOMBRES DEL EMPLEADO SON: ".$NomApellidos;
+hecho "EL CARGO DEL EMPLEADO ES: ".$Cargo;
+hecho "EL SALARIO DEL EMPLEADO ES: ".$Salario;
+?>
